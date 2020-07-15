06d65fda3ae1448a49d55f51aa5ffb078f1e25a5
박호준 <hankil417@naver.com>
2020-07-15 21:05:10 +0900

    프래그먼트에서 결과를 가질러간다

--- app/src/main/java/kr/co/tjoeun/pizzastore_20200714/fragments/MyProfileFragment.kt
+++ app/src/main/java/kr/co/tjoeun/pizzastore_20200714/fragments/MyProfileFragment.kt
@@ -1,13 +1,20 @@
 package kr.co.tjoeun.pizzastore_20200714.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_my_profile.*
+import kr.co.tjoeun.pizzastore_20200714.EditNickNameActivity
 import kr.co.tjoeun.pizzastore_20200714.R
 
 class MyProfileFragment : Fragment() {
+
+    //닉네임을 가지러 간다고 명시하는 숫자를 담은 변수
+    val  REQ_FOR_NICKNAME
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -17,4 +24,18 @@ class MyProfileFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_my_profile, container, false)
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        changeNickNameBtn.setOnClickListener {
+
+            val myIntent = Intent(activity,EditNickNameActivity::class.java)
+
+            startActivityForResult(myIntent,REQ_FOR_NICKNAME)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+    }
 }
